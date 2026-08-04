3cf3e187133e61c0183f33131a0197e28cd41df4
Mizodeus <151405871+Mizodeus@users.noreply.github.com.>
2026-08-04 20:37:19 +0500

    Add card display

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const library = [];
 
 const DEFAULT = [
-    {name: "Fight Club", pages: "224", isRead: true, author: "Chuck Palahniuk"},
-    {name: "The Witcher: The Last Wish", pages: "288", isRead: true, author: "Andrzej Sapkowski"}
-]
+    {title: "Fight Club", pages: "224", isRead: true, author: "Chuck Palahniuk"},
+    {title: "The Witcher: The Last Wish", pages: "288", isRead: false, author: "Andrzej Sapkowski"}
+];
+
+const container = document.querySelector('.container');
 
 function Book(title, pages, isRead, author) {
     this.title = title;
@@ -18,3 +20,35 @@ function addBookToLibrary(title, pages, isRead, author) {
     library.push(book);
 };
 
+library.push(...DEFAULT);
+
+library.forEach(book => {
+    const card = document.createElement('div');
+    card.className = 'book-card';
+
+    const title = document.createElement('p');
+    title.textContent = book.title;
+
+    const pages = document.createElement('p');
+    pages.textContent = book.pages;
+
+    const author = document.createElement('p');
+    author.textContent = book.author;
+
+    const btn = document.createElement('button');
+
+    function updateButton() {
+        btn.classList.toggle('active', book.isRead);
+        btn.textContent = book.isRead ? 'Read' : 'Not Read';
+    }
+
+    updateButton();
+
+    btn.addEventListener('click', () => {
+        book.isRead = !book.isRead;
+        updateButton();
+    });
+
+    card.append(title, pages, author, btn)
+    container.append(card)
+});
