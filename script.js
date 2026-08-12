5a546a0e8536f8ef95582c84d9b079e52bedc806
Mizodeus <151405871+Mizodeus@users.noreply.github.com.>
2026-08-12 16:58:39 +0500

    Add cards from a form

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ const DEFAULT = [
 ];
 
 const container = document.querySelector('.container');
-const addBookButton = document.querySelector('.addBook');
-const addBookDialog = document.querySelector('#addBookDialog');
-const cancelButton = document.querySelector('#cancelButton');
+const addBookBtn = document.querySelector('.addBook');
+const dialog = document.querySelector('#addBookDialog');
+const form = document.querySelector('#addBookForm')
+const cancelBtn = document.querySelector('#cancelButton');
 
 function Book(title, pages, isRead, author) {
     this.title = title;
@@ -23,43 +24,68 @@ function addBookToLibrary(title, pages, isRead, author) {
     library.push(book);
 };
 
-addBookButton.addEventListener('click', () => {
-    addBookDialog.showModal();
+function renderBooks() {
+    container.innerHTML = '';
+
+    library.forEach(book => {
+        const card = document.createElement('div');
+        card.className = 'book-card';
+
+        const title = document.createElement('p');
+        title.textContent = book.title;
+
+        const pages = document.createElement('p');
+        pages.textContent = book.pages;
+
+        const author = document.createElement('p');
+        author.textContent = book.author;
+
+        const btn = document.createElement('button');
+
+        function updateButton() {
+            btn.classList.toggle('active', book.isRead);
+            btn.textContent = book.isRead ? 'Read' : 'Not Read';
+        }
+
+        updateButton();
+
+        btn.addEventListener('click', () => {
+            book.isRead = !book.isRead;
+            updateButton();
+        });
+
+        card.append(title, pages, author, btn)
+        container.append(card)
+    });
+}
+
+addBookBtn.addEventListener('click', () => {
+    dialog.showModal();
 })
 
-cancelButton.addEventListener('click', () => {
-    addBookDialog.close()
+cancelBtn.addEventListener('click', () => {
+    form.reset();
+    dialog.close()
 })
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    addBookToLibrary(
+        data.get('title'),
+        Number(data.get('pages')),
+        form.isRead.checked,
+        data.get('author')
+    );
+
+    renderBooks();
+
+    form.reset();
+    dialog.close();
+});
 
 library.push(...DEFAULT);
 
-library.forEach(book => {
-    const card = document.createElement('div');
-    card.className = 'book-card';
-
-    const title = document.createElement('p');
-    title.textContent = book.title;
-
-    const pages = document.createElement('p');
-    pages.textContent = book.pages;
-
-    const author = document.createElement('p');
-    author.textContent = book.author;
-
-    const btn = document.createElement('button');
-
-    function updateButton() {
-        btn.classList.toggle('active', book.isRead);
-        btn.textContent = book.isRead ? 'Read' : 'Not Read';
-    }
-
-    updateButton();
-
-    btn.addEventListener('click', () => {
-        book.isRead = !book.isRead;
-        updateButton();
-    });
-
-    card.append(title, pages, author, btn)
-    container.append(card)
-});
+renderBooks();
